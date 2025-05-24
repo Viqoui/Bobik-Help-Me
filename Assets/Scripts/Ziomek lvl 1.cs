683c10f8e65db1298b62734a98ea654648e5921c
Paanwel <paanwel123@gmail.com>
2025-05-25 00:34:36 +0200

    lvl1 poprawka dialogi

--- Assets/Scripts/Ziomek lvl 1.cs
+++ Assets/Scripts/Ziomek lvl 1.cs
@@ -10,7 +10,7 @@ public class Ziomeklvl1 : MonoBehaviour
 {
     public bool playerIsClose;
     private bool dialogend;
-    public GameObject popo;
+    
     public KotNPC npc;
     // Start is called before the first frame update
     void Start()
