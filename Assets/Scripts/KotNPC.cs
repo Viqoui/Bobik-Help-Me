219c46d90b7af34f69f6dbbc896b575617399b71
Paanwel <paanwel123@gmail.com>
2025-05-24 23:58:23 +0200

    commit 1 z 2

--- Assets/Scripts/KotNPC.cs
+++ Assets/Scripts/KotNPC.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class KotNPC : MonoBehaviour
 {
@@ -10,12 +12,13 @@ public class KotNPC : MonoBehaviour
     public bool szukanie;
     public bool zakonczone;
     private bool ol;
-    public GameObject dialog;
-    public GameObject dialog2;
+    public GameObject kot1;
+    public GameObject kot2;
     public GameObject BOBIK1;
     public GameObject BOBIK2;
     
     public GameObject Kot3;
+    public GameObject nowosc;
     public GameObject Objective;
     public GameObject Objective1;
     public GameObject Objective2;
@@ -53,7 +56,7 @@ public class KotNPC : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.E) && playerIsClose == true && krzak.kotowraca == true && ol == true)
         {
-            dialog.SetActive(true);
+            kot2.SetActive(true);
             krzak.Panel2.SetActive(false);
             ol = false;
         }
@@ -81,8 +84,8 @@ public class KotNPC : MonoBehaviour
     }
     public void close1()
     {
-        dialog.SetActive(false);
-        dialog2.SetActive(true);
+        kot1.SetActive(false);
+        kot2.SetActive(true);
         
     }
 
@@ -90,10 +93,11 @@ public class KotNPC : MonoBehaviour
     {   
         BOBIK1.SetActive(false);
         Kot3.SetActive(true);
+        StartCoroutine(czk());
     }
     public void close3()
     {
-        Kot3.SetActive(false);
+        nowosc.SetActive(false);
         BOBIK2.SetActive(true);
     }
     public void close4()
@@ -104,9 +108,16 @@ public class KotNPC : MonoBehaviour
     }
     public void close11()
     {
-        dialog2.SetActive(false);
+        kot2.SetActive(false);
         zakonczone = true;
         Objective2.SetActive(true);
+    }
+    IEnumerator czk()
+    {
+        yield return new WaitForSeconds(3);
+        Kot3.SetActive(false);
+        nowosc.SetActive(true);
+
     }
 
 }
